2f7e3f02899f37fd87fadfa82ff72c85a1dfeb60
kpganvir <kalps.ganvir@gmail.com>
2022-09-17 01:45:07 +0530

    added one more test case

--- sample_hardhat_example/test/token.js
+++ sample_hardhat_example/test/token.js
@@ -5,7 +5,7 @@ const { expect } = require("chai");
 describe("Token contract" ,function(){
 
 
-it(" Deployment should assing total supply to the owner",async function(){
+it(" Deployment should assign total supply to the owner",async function(){
 
    
     const Token =await ethers.getContractFactory("Token"); //instance of contract
@@ -17,6 +17,23 @@ it(" Deployment should assing total supply to the owner",async function(){
     
     
     expect(Number(_ownerBalance)).to.equal(Number(_totalSupply));
+
+});
+
+it(" Deployment should trasfer amount to given address frm owener balace",async function(){
+
+   
+    const Token =await ethers.getContractFactory("Token"); //instance of contract
+    const hardhatToken =await Token.deploy(); //deploying here, getting object
+    const[owner,add1]=await ethers.getSigners();
+
+    
+    await hardhatToken.transfer(add1.address,10);
+    const _add1Balance=await hardhatToken.balanceOf(add1.address);
+    const _owmerBal=await hardhatToken.balanceOf(owner.address);
+    
+    expect(Number(_add1Balance)).to.equal(10);
+    expect(Number(_owmerBal)).to.equal(990);
 
 });
 
